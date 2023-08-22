162145ff93cd8c69ff3934e60aaec0623df8f534
LangPhan <langphankk@gmail.com>
2023-08-22 21:36:49 +0700

    FE: Routes-Layouts-Login-Register-Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import AppLayout from "./layout/AppLayout";
+import NotFoundPage from "./screens/NotFound";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<AppLayout />} errorElement={<NotFoundPage />}>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+      </Route>
+    )
+  );
   return (
     <>
-      <p className="text-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   );
 }
